a8bc05c5a9d9523d9a181bff0cf4d6377ac1ac74
phelype Ferreira <phelype_dipaula@hotmail.com>
2022-05-15 19:56:21 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,43 +1,44 @@
 import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
 
-    // FORMA ERRADA
-
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(response => {
-        console.log(response.data);
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`).then((response) => {
+      const data = response.data as MoviePage;
+      console.log(data);
+      setPageNumber(data.number);
     });
+  }, []);
 
-    return(
-        <>
-        <Pagination />
+  return (
+    <>
+      <p>{pageNumber}</p>
+      <Pagination />
 
-        <div className="container">
-
+      <div className="container">
         <div className="row">
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                <MovieCard />
-            </div>
-            
-         </div>   
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
         </div>
-        
-
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Listing;
